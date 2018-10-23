35e6ce44182d1630f5ae0a4391027baae9c63935
Robbert Wethmar <robbert@digitalica.nl>
2018-10-23 16:01:37 +0200

    Minor fix in documentation of handleRequest

--- src/dialogflow-fulfillment.js
+++ src/dialogflow-fulfillment.js
@@ -292,8 +292,9 @@ class WebhookClient {
    * Handles the incoming Dialogflow request using a handler or Map of handlers
    * Each handler must be a function callback.
    *
-   * @param {Map|requestCallback} handler map of Dialogflow action name to handler function or
+   * @param {Map|requestCallback} handler map of Dialogflow intent name to handler function or
    *     function to handle all requests (regardless of Dialogflow action).
+   *     In an intent map, a null can map to a default handler.
    * @return {Promise}
    */
   handleRequest(handler) {
